e5e191a6656e44d8a16cef810ebad78fdb45d763
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-23 18:40:01 -0300

    OmniStack 11 - Criando Hello World!

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(request, response){
+    return response.send("Hello World.");
+})
+
 app.listen(3333, function(){
-    console.log("Servidor rodando na porta 3333.")
+    console.log("Servidor rodando na porta 3333.");
 });
