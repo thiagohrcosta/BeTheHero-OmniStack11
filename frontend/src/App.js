00326200d9bdd7c5387af7ace77b2c3c546c6d46
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-23 19:28:29 -0300

    OmniStack 11 - Criando Hello World no REACT

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>OmniStack - 2020</h1>
+          Projeto - Be The Hero
         </p>
         <a
           className="App-link"
