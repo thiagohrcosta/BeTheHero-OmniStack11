923b3d894a889197cfaa330cb5ff747a2c426685
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-23 18:42:35 -0300

    OmniStack 11 - JSON Response

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(request, response){
-    return response.send("Hello World.");
-})
+    return response.json({
+        evento: "Semana OmniStack 11.0",
+        aluno: "Thiago Costa"
+    });
+});
 
 app.listen(3333, function(){
     console.log("Servidor rodando na porta 3333.");
