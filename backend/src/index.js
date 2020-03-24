4e7c526d72805b0158407872734a402d3125e627
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-24 20:27:54 -0300

    OmniStack 11 - Finalizando BACKEND e Aula 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,43 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.use(routes);
-
-/** ======================================
- * Rota / Recurso
- */
-
-/** ======================================
- * Métodos HTTP:
- * 
- * GET: Buscar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Apagar uma informação no back-end
- */
-
-/** ======================================
-  * Tipos de parâmetros:
-  * 
-  * Query: Parâmetros nomeados enviados na rota após
-  *        o símbolo de ? e servem geralmente para
-  *        filtros, paginação. 
-  * 
-  * Route: Parâmetros utilizados para identificar
-  *        recursos.
-  * 
-  * Request Body: É o corpo da requisição, utilizado
-  *               para criar ou alterar recursos.
-  */
-
-  /**
-   * Driver : SELECT * FROM users
-   * Query Builder: table('users').select('*').where()
-   */
+app.use(routes); 
 
 
 app.listen(3333, function(){
