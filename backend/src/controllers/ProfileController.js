831c304bf061ca84accc4a36c7c6f4ac2de0495e
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-24 20:03:41 -0300

    OmniStack 11 - Criando rota de login

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,7 +1,7 @@
 const connection = require("../database/connection");
 
 module.exports = {
-    async indexedDB(request, response){
+    async index(request, response){
         const ong_id = request.headers.authorization;
 
         const incidents = await connection("incidents")
