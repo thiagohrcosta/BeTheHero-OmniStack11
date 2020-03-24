7ecd5d5a0ea030c953862a9164ea5f7641266a0b
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-24 15:13:02 -0300

    OmniStack 11 - INICIANDO AULA 2

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const app = express();
 
+
+/** ======================================
+ * Rota / Recurso
+ */
+
+/** ======================================
+ * Métodos HTTP:
+ * 
+ * GET: Buscar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Apagar uma informação no back-end
+ */
+
 app.get("/", function(request, response){
     return response.json({
         evento: "Semana OmniStack 11.0",
