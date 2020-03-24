ac9ff48f1b1b2338f1ae24a4df7978d2e5d74de9
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-24 19:43:24 -0300

    OmniStack 11 - Finalizando rota DELETE

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,6 +1,12 @@
 const connection = require("../database/connection");
 
 module.exports = {
+    async index(request, response){
+        const incidents = await connection("incidents").select("*");
+
+        return response.json(incidents);
+    },
+
     async create(request, response){
         const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
@@ -14,5 +20,22 @@ module.exports = {
 
         return response.json({ id });
 
+    },
+
+    async delete(request, response){
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+        
+        const incident = await connection("incidents")
+            .where("id", id)
+            .select("ong_id")
+            .first();
+
+        if (incident.ong_id != ong_id){
+            return response.status(401).json({ error: 'Operation not permited. '});
+        }
+        await connection("incidents").where("id", id).delete();
+
+        return response.status(204).send();
     }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,9 @@ const routes = express.Router();
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
 
+routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.create);
+
+routes.delete("/incidents/:id", IncidentController.delete);
 
 module.exports = routes;
