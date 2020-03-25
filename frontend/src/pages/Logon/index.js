a126eba13fd85855869e7f70534f457301512d0e
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-25 19:24:59 -0300

    OmniStack 11 - Finalizando estilização da página de LOGIN

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
@@ -13,13 +15,15 @@ export default function Logo(){
                 <form action="">
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID" />
-                    <button type="submit">Entrar</button>
+                    <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register">Não tenho cadastro</a>
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro</a>
                 </form>
             </section>
 
-            <img src={heroesImg} alt="Heroes" />>
+            <img src={heroesImg} alt="Heroes" />
         </div>
     );
 }
