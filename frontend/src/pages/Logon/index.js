a5e6e2b056e6b4259a1818c73642c5c8425cf87f
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-25 19:07:58 -0300

    OmniStack 11 - Instalando React Icons

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import './styles.css'
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
 
 export default function Logo(){
     return(
-        <h1>Hello World!</h1>
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+
+                <form action="">
+                    <h1>Faça seu logon</h1>
+                    <input placeholder="Sua ID" />
+                    <button type="submit">Entrar</button>
+
+                    <a href="/register">Não tenho cadastro</a>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="Heroes" />>
+        </div>
     );
 }
