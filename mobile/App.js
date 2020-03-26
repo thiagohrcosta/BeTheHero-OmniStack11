1dafd1bb992f41ad6bb26ac2deb596dd3ab28f15
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-26 19:51:09 -0300

    OmniStack 11 - Iniciando páginas MOBILE

--- mobile/App.js
+++ mobile/App.js
@@ -3,21 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello OmniStack!</Text>
+    <View>
+      <Text>Hello OmniStack!</Text>
     </View>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f34553',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title : {
-    color: "white",
-    fontSize: 30,
-  },
-});
+}
