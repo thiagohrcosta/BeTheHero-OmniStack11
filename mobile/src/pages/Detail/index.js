2e12b8527d9b9be3b056d8b2061d1931bd60293f
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-27 13:16:47 -0300

    OmniStack 11 - Adicionando botões de contato

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+import * as MailComposer from 'expo-mail-composer';
+
 
 import logoImg from "../../assets/logo.png";
 
@@ -9,9 +11,23 @@ import styles from "./styles";
 
 export default function Detail() {
     const navigation = useNavigation();
+    const message = 'Olá HelpCat, estou entrando em contato para ajudar no caso "Alimentos" com o valor de "R$ 120,00".';
 
     function navigateBack(){
         navigation.goBack();
+    }
+
+    function sendMail(){
+        MailComposer.composeAsync({
+            subject: 'Herói do caso: Alimentos',
+            recipients: ['teste@teste.com.br'],
+            body: message,
+        })
+
+    }
+
+    function sendWhatsapp(){
+        Linking.openURL(`whatsapp://send?phone=0000000000&text=${message}`);
     }
 
 
@@ -42,10 +58,10 @@ export default function Detail() {
           <Text style={styles.heroDescription}>Entre em contato.</Text>
 
           <View style={styles.actions}>
-              <TouchableOpacity style={styles.action} onPress={() => {}} >
+              <TouchableOpacity style={styles.action} onPress={sendWhatsapp} >
                   <Text style={styles.actionText}>WhatsApp</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.action} onPress={() => {}} >
+              <TouchableOpacity style={styles.action} onPress={sendMail} >
                   <Text style={styles.actionText}>E-mail</Text>
               </TouchableOpacity>
           </View>
